ab577562311d6ac73a329bd78904febda20426e0
dflesh <dflesh1111@yandex.ru>
2019-10-03 02:33:24 +0300

    +create bd tables

--- db.py
+++ db.py
@@ -1,35 +1,58 @@
 import sqlite3
-
-connection = sqlite3.connect('VkUserAnalyse.sqlite')
-
-#открываем соединение
-cursor = connection.cursor()
-
-#cоздание таблицы группы вятгу
-cursor.execute(""""Create table Group
-                    """)
-
-#таблица пользователей
-cursor.execute("""Crate table User
-               """)
-
-#таблица групп пользователей
+from datetime import datetime, date, time
 
 
-#таблица возрастов
+def create_db():
+    year_now = str(datetime.now().year)
+    month_now = str(datetime.now().month)
+    day_now = str(datetime.now().day)
+    date_now = year_now + '_' + month_now + '_' + day_now
+
+    db_name = str(date_now) + '.db'
+    print(db_name)
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
+    cursor.execute('CREATE TABLE VSU_Community'
+                   '(id integer primary key, '
+                   'name text, '
+                   'info text, '
+                   'href text)')
+
+    cursor.execute('CREATE TABLE VSU_Member'
+                   '(id integer primary key, '
+                   'name varchar(100), '
+                   'gender varchar(10), '
+                   'age integer, '
+                   'href text)')
+
+    cursor.execute('CREATE TABLE VSU_Member_Activity'
+                   '(like integer,'
+                   'repost integer,'
+                   'comment integer,'
+                   'postID integer,'
+                   'memberID integer,'
+                   'communityID integer, '
+                   'foreign key(memberID) references VSU_Member(id),'
+                   'foreign key(communityID) references VSU_Community(id))')
+
+    cursor.execute('CREATE TABLE VSU_Member_Community'
+                   '(memberID integer,'
+                   'communityID integer, '
+                   'href text, '
+                   'name text,'
+                   'foreign key(memberID) references VSU_Member(id))')
 
 
-#таблица лайков
+    # cursor.execute('INSERT INTO vyatsu_group VALUES("yellow", "black")')
+    # cursor.execute('SELECT * FROM VSU_Group')
+
+    #print(cursor.fetchall())
+
+    connection.commit()
+    connection.close()
 
 
-#таблица репостов
+def main():
+    create_db()
 
-
-#таблица комментариев
-
-
-
-
-
-#закрываем соединение
-connection.close()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import vk
+import db
 
 session = vk.Session(access_token='d5b441ccd5b441ccd5b441cc0bd5d94752dd5b4d5b441cc883ce57ed215c145977b71cd')
 api = vk.API(session)
@@ -10,7 +11,6 @@ def get_users():
     members = []
     print(resp['items'])
     offset = 0
-    j = 0
     count = 1
     while offset < 1001:
         resp = api.groups.getMembers(group_id='prcom_vyatsu', v=v, offset=offset)
@@ -18,9 +18,12 @@ def get_users():
         for i in resp['items']:
             print(count)
             count += 1
-            members.append(api.users.get(user_ids=i, v=v))
-            if count > 100:
+            members.append(api.users.get(user_ids=i, v=v, fields='bdate'))
+            #print(api.getProfile.get(user_ids=i, v=v, profile_fields='all'))
+            if count > 5:
                 break
 
     print(*members, sep="\n")
+
 get_users()
+db.main()
