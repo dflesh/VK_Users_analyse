29f76884af8db4a9c1eaa75a4c1f9c0fdd925590
Sergeres <sergius.jr@mail.ru>
2019-11-03 14:22:14 +0300

    jinja file add

--- jinja.py
+++ jinja.py
@@ -1,22 +1,8 @@
+# -*- coding: utf-8 -*-
 import sqlite3
+import pandas as pand
+import matplotlib.pyplot as plt
 from jinja2 import Environment, FunctionLoader, PackageLoader, PrefixLoader, DictLoader, FileSystemLoader
-
-html = '''
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  </head>
-  <body>
-      Hello
-      <br>
-      {% for user in members %}
-        <li>{{ user }}</li>
-      {% endfor %}
-      <br>
-  </body>
-</html>
-'''
 
 
 def create_conn(db_file):
@@ -29,31 +15,111 @@ def select_mem(conn):
     rows = []
     cur = conn.cursor()
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age < 18")
-    rows.append(cur.fetchall())
-    print(type(cur.fetchall()))
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age >= 18 and age < 21")
-    rows.append(cur.fetchall())
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age >= 21 and age < 24")
-    rows.append(cur.fetchall())
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age >= 24 and age < 27")
-    rows.append(cur.fetchall())
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age >= 27 and age < 30")
-    rows.append(cur.fetchall())
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age >= 30 and age < 35")
-    rows.append(cur.fetchall())
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age >= 35 and age < 45")
-    rows.append(cur.fetchall())
+    rows.append(cur.fetchall()[0])
     cur.execute("SELECT count(name) FROM VSU_Member WHERE age > 45")
-    # rows = cur.fetchall()
-    rows.append(cur.fetchall())
-
-    print(rows)
+    rows.append(cur.fetchall()[0])
+    rows = list(sum(rows, ()))
+    summa = sum(rows)
+    x_row = ["< 18","18-21","21-24","24-27","27-30","30-35","35-45","> 45"]
+    for i in range(rows.__len__()):
+        rows[i] = rows[i] * 100 / summa
+    dataframe = pand.DataFrame()
+    dataframe['Проценты'] = rows
+    dataframe['Категории'] = x_row
+    agraph = dataframe.plot(x = 'Категории', kind = 'bar', color = 'teal')
+    agraph.set(xlabel = "Категории возрастов", ylabel = "Проценты")
+    plt.tight_layout()
+    plt.savefig('templates/screenshots/categoryGroups.png')
     return rows
 
 
-env = Environment(loader = DictLoader({'index.html': html}))
-template = env.get_template('index.html')
-# print(template.render(name=members[0], photo_50 = member_communities[0]))
-# conn = create_connection(r"D:\5th Semestr\MMAD\vkAnalyse\vk_members_2019_10_21.db")
-with open("new.html", "w") as f:
-    f.write(template.render(members = select_mem(create_conn(r"D:\\5th Semestr\\MMAD\\vkAnalyse\\vk_members_2019_10_22.db"))))
+def top_communityM(conn, gen):
+    cur = conn.cursor()
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data = cur.fetchall()
+    name =  []
+    count = []
+    for row in data:
+        name.append(row[0])
+        count.append(row[1])
+    dataframe = pand.DataFrame()
+    dataframe["Имена"] = name
+    dataframe["Количество"] = count
+    cgraph = dataframe.plot(x = 'Имена', kind = 'bar', color = 'c')
+    cgraph.set(xlabel = "Названия групп", ylabel = "Количество")
+    plt.tight_layout()
+    if gen == 'Муж.':
+        plt.savefig('templates/screenshots/mensTOP5.png')
+    else:
+        plt.savefig('templates/screenshots/womensTOP5.png')
+
+
+def top_ages(conn, gen):
+    data = []
+    cur = conn.cursor()
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age < 18 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 18 and VSU_Member.age < 21 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 21 and VSU_Member.age < 24 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 24 and VSU_Member.age < 27 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 27 and VSU_Member.age < 30 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 30 and VSU_Member.age < 35 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 35 and VSU_Member.age < 45 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    cur.execute("select VSU_Member_Community.name, count(*) from VSU_Member_Community join VSU_Member on VSU_Member_Community.memberID = VSU_Member.id where VSU_Member.gender =:gen and VSU_Member.age >= 45 group by VSU_Member_Community.name order by count(*) DESC limit 5", {"gen": gen})
+    data.append(cur.fetchall())
+    x_row = ["меньше 18", "от 18 до 21", "от 21 до 24", "от 24 до 27", "от 27 до 30", "от 30 до 35", "от 35 до 45", "больше 45"]
+    graphs = []
+    for i in range(data.__len__()):
+        name = []
+        count = []
+        for row in data[i]:
+            name.append(row[0])
+            count.append(row[1])
+        dataframe = pand.DataFrame()
+        dataframe["Имена"] = name
+        dataframe["Количество"] = count
+        if gen == 'Жен.':
+            cgraph = dataframe.plot(x='Имена', kind='bar', color='c', title  = "Женщины " + x_row[i])
+            cgraph.set(xlabel="Названия групп", ylabel="Количество")
+            plt.tight_layout()
+            plt.savefig('templates/screenshots/top5_W'+str(i)+'.png')
+            graphs.append('screenshots/top5_W'+str(i)+'.png')
+        else:
+            cgraph = dataframe.plot(x='Имена', kind='bar', color='c', title="Мужчины " + x_row[i])
+            cgraph.set(xlabel="Названия групп", ylabel="Количество")
+            plt.tight_layout()
+            plt.savefig('templates/screenshots/top5_M' + str(i) + '.png')
+            graphs.append('screenshots/top5_M' + str(i) + '.png')
+    return graphs
+
+
+env = Environment(loader = FileSystemLoader('templates/'))
+template = env.get_template('templateRE.html')
+
+select_mem(create_conn(r"D:\\5th Semestr\\MMAD\\vkAnalyse\\vk_members_2019_10_22.db"))
+top_communityM(create_conn(r"D:\\5th Semestr\\MMAD\\vkAnalyse\\vk_members_2019_10_22.db"), 'Жен.')
+top_communityM(create_conn(r"D:\\5th Semestr\\MMAD\\vkAnalyse\\vk_members_2019_10_22.db"), 'Муж.')
+graphs = top_ages(create_conn(r"D:\\5th Semestr\\MMAD\\vkAnalyse\\vk_members_2019_10_22.db"), 'Жен.')
+tgraphs = top_ages(create_conn(r"D:\\5th Semestr\\MMAD\\vkAnalyse\\vk_members_2019_10_22.db"), 'Муж.')
+
+with open("templates/new.html", "w") as f:
+    f.write(template.render(url1 = 'screenshots/categoryGroups.png', url2 = 'screenshots/womensTOP5.png', url3 = 'screenshots/mensTOP5.png', mems = graphs))
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import vk
 import db
-from jinja2 import Environment, FunctionLoader, PackageLoader, PrefixLoader, DictLoader, FileSystemLoader
+import time
 
 session = vk.Session(access_token='d5b441ccd5b441ccd5b441cc0bd5d94752dd5b4d5b441cc883ce57ed215c145977b71cd')
 api = vk.API(session)
@@ -8,73 +8,110 @@ v = 5.101
 
 
 def get_users():
-    resp = api.groups.getMembers(group_id='prcom_vyatsu', v=v, offset='0')
+    response = api.groups.getMembers(group_id='prcom_vyatsu', v=v, offset='0')
     members = []
     offset = 0
     count = 1
-    while offset < resp['count']:
-        resp = api.groups.getMembers(group_id='prcom_vyatsu', v=v, offset=offset)
+    while offset < response['count']:
+        response = api.groups.getMembers(group_id='prcom_vyatsu', v=v, offset=offset)
         offset += 1000
-        for i in resp['items']:
-            print(count)
+        for member_id in response['items']:
             count += 1
-            members.append(api.users.get(user_ids=i, v=v, fields='bdate, sex'))
-            if count > 10:
-                break
+            members.append(api.users.get(user_ids=member_id, v=v, fields='bdate, sex'))
+            # if count > 10:
+            #     break
     return members
 
 
 def get_communities(members):
     member_communities = []
-    for i in members:
-        for j in i:
-            id = j['id']
+    for member in members:
+        # print(member)
+        for info in member:
+            member_id = info['id']
             try:
-                response = api.users.getSubscriptions(user_id=id, v=v, extended=1)
-                print(response)
+                response = api.users.getSubscriptions(user_id=member_id, v=v, extended=1)
                 subscriptions = response['items']
-                all_groups = []
-                for group in subscriptions:
-                    print(group)
-                    name = group['name']
-                    print(name)
-                    groupID = group['id']
+                all_communities = []
+                for community in subscriptions:
+                    community_name = community['name']
+                    community_id = community['id']
 
-                    all_groups.append({'id': groupID, 'name': name})
-                member_communities.append({'id': id, 'subscriptions': all_groups})
+                    all_communities.append({'id': community_id, 'name': community_name})
+                member_communities.append({'id': member_id, 'subscriptions': all_communities})
             except:
                 print('profile is private')
-    print(*member_communities, sep='\n')
-
     return member_communities
 
 
-html = '''<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  </head>
-  <body>
-      Hello
-      <br>
-      {{ name }}!
-      <br>
-  </body>
-</html>
-'''
+def get_vsu_group():
+    vsu_group = api.groups.getById(group_id='prcom_vyatsu', v=v, fields='description')
+    return vsu_group
+
+
+def get_vsu_posts():
+    counter = 0
+    offset = 0
+    response = api.wall.get(owner_id='-108366262', v=v, offset=offset, filter='owner')
+    count = response['count']
+    # print(count)
+    posts = {}
+    while offset < count:
+        response = api.wall.get(owner_id='-108366262', v=v, count=count, offset=offset, filter='owner')
+        offset += 100
+        for post in response['items']:
+            # print(post)
+            posts[counter] = post['id']
+            counter += 1
+            if counter > count:
+                break
+    return posts
+
+
+def get_activity(posts, users_ids):
+    activity = []
+    for i in posts:
+        post_id = posts[i]
+        # print(post_id)
+        likes_list = api.likes.getList(type='post', item_id=post_id, v=v, filter='likes', owner_id='-108366262')
+        comments_list = api.wall.getComments(owner_id='-108366262', post_id=post_id, v=v)
+        # print(comments_list)
+        # print(likes_list)
+        if likes_list['count'] != 0:
+            for like in likes_list['items']:
+                for comment in comments_list['items']:
+                    # print(comment)
+                    for user_id in users_ids:
+                        if user_id == like:
+                            like_flag = 1
+                        else:
+                            like_flag = 0
+                        if user_id == comment['from_id']:
+                            comment_flag = 1
+                        else:
+                            comment_flag = 0
+                        if like_flag == 1 or comment_flag == 1:
+                            activity.append({'userID': user_id, 'postID': post_id,
+                                             'like': like_flag, 'comment': comment_flag})
+    # print(*activity, sep='\n')
+    return activity
 
 
 def main():
     db.create_tables()
     members = get_users()
-    member_communities = get_communities(members)
     db.members_insert(members)
+    member_communities = get_communities(members)
+    db.member_community_insert(member_communities)
+    vsu_group = get_vsu_group()
+    db.vsu_community_insert(vsu_group)
+    users = db.select_users_ids()
+    posts = get_vsu_posts()
+    activities = get_activity(posts, users)
+    db.insert_activities(activities)
 
-    env = Environment(loader = DictLoader({'index.html': html}))
-    template = env.get_template('index.html')
-    # print(template.render(name=members[0], photo_50 = member_communities[0]))
-    with open("new.html", "w") as f:
-        f.write(template.render(name=members[1]))
 
-
+start_time = time.time()
+print(start_time)
 main()
+print(time.time() - start_time)
